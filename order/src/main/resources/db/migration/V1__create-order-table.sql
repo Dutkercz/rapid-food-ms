062e80a08cf69484a1d21ce7e2003ff39d5fe1bf
Dutkercz <dutkercz@gmail.com>
2026-07-10 10:09:26 -0300

    fix: Update order and payment status column names in SQL script for clarity

--- order/src/main/resources/db/migration/V1__create-order-table.sql
+++ order/src/main/resources/db/migration/V1__create-order-table.sql
@@ -3,9 +3,9 @@ CREATE TABLE orders(
     total_amount DECIMAL(10,2),
     observation VARCHAR(255),
     order_status VARCHAR(30) NOT NULL CHECK (
-        status IN ('CREATED', 'SENT_TO_VENDOR', 'ACCEPTED', 'PREPARING', 'DELIVERED', 'CANCELED')),
+        order_status IN ('CREATED', 'SENT_TO_VENDOR', 'ACCEPTED', 'PREPARING', 'DELIVERED', 'CANCELED')),
     payment_status VARCHAR(30) NOT NULL CHECK (
-        status IN ( 'PENDING', 'APPROVED', 'REJECTED', 'CANCELLED', 'REFUNDED')),
+        payment_status IN ( 'PENDING', 'APPROVED', 'REJECTED', 'CANCELLED', 'REFUNDED')),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     user_id BIGINT NOT NULL,
